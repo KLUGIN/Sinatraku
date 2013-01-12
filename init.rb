8b6e30ca69cebfb5d44ee00fde44eef857eb8535
Javier Carballo <javier@javier.im>
2013-01-11 21:40:27 -0500

    Added Bootstrap and jquery

--- init.rb
+++ init.rb
@@ -1,10 +1,3 @@
-# You'll need to require these if you
-# want to develop while running with ruby.
-# The config/rackup.ru requires these as well
-# for it's own reasons.
-#
-# $ ruby init.rb
-#
 require 'rubygems'
 require 'sinatra'
 
@@ -20,13 +13,3 @@ end
 get '/' do
   "I'm Here Baby!"
 end
-
-# Test at <appname>.heroku.com
-
-# You can see all your app specific information this way.
-# IMPORTANT! This is a very bad thing to do for a production
-# application with sensitive information
-
-# get '/env' do
-#   ENV.inspect
-# end
